b42132ce5a3a0f5363d25b4df7ca388899de616f
Adrián Núñez <anunezpedroche@gmail.com>
2020-02-03 22:49:50 +0100

    Añadiendo drag and drop

--- AJAX/Examen19/js/scriptCrear.js
+++ AJAX/Examen19/js/scriptCrear.js
@@ -7,12 +7,31 @@ function init (){
     let imagen = document.getElementById('imagen');
 
     document.activeElement.addEventListener('focusout',comprobarVacio);
-    document
+    document.querySelector('button').addEventListener('click',crearPersonajo);
 }
 
 function crearPersonajo(){
-    if(nombre&&apellidos&&casa&&padres&&titulo&&titulo&&imagen){
-        console.log('hola');a
+  
+    if(nombre.value&&apellidos.value&&casa.value&&padres.value&&titulo.value&&titulo.value&&imagen.value){
+    let contenedorFichas = document.getElementById('contenedorFichas');
+    let img = document.createElement('img');
+    img.src = imagen.value;
+    img.classList.add('foto');
+    let datos = document.createElement('div');
+    datos.classList.add('datos');
+    datos.innerHTML =
+        `<div class="small cabecera">Nombre</div>
+        <div class="medium cabecera">Apellidos</div>
+        <div class="small dato">${nombre.value}</div>
+        <div class="medium dato">${apellidos.value}</div>
+        <div class="medium cabecera">Padres</div>
+        <div class="small cabecera">Casa</div>
+        <div class="medium dato">${padres.value}</div>
+        <div class="small dato">${casa.value}</div>
+        <div class="big cabecera">Título</div>
+        <div class="big dato">${titulo.value}</div>`;
+        contenedorFichas.appendChild(img);
+        contenedorFichas.appendChild(datos);
     }
 }
 
--- AJAX/Examen19/js/scriptJugar.js
+++ AJAX/Examen19/js/scriptJugar.js
@@ -1,7 +1,67 @@
 function recuperarJSON() {
-    return fetch('http://192.168.1.144:8000/AJAX/Examen19/got.json').then(
+    return fetch('http://localhost:8000/AJAX/Examen19/got.json').then(
         response => response.json())
         .then(got => {
-            miGot = got;
+            crearPlantilla(got);
         });
 }
+
+function crearPlantilla(gotJSON){
+    let divPrincipal = document.querySelector('#contenedorFotos');
+    gotJSON.got.forEach(personaje => {
+        divPrincipal.innerHTML+=`
+        <div class="col-xs-6 col-sm-3 contenedorJuego" id="${personaje.nombre}" draggable="true">
+        <img class="img-thumbnail" id="${personaje.familia}" src="${personaje.imagen}" draggable="false" ></img>
+        <select class="form-control">
+        <option>Selecciona familia...</option>
+        <option value="Casa Lannister">Casa Lannister</option>
+        <option value="Casa Targaryen">Casa Targaryen</option>
+        <option value="Casa Stark">Casa Stark</option>
+        <option value="Casa Bolton">Casa Bolton</option>
+        <option value="Casa Tyrell">Casa Tyrell</option>
+        <option value="Casa Baratheon">Casa Baratheon</option>
+        <option value="Casa Clegane">Casa Clegane</option>
+        </select>
+        </div>`;
+    });
+}
+
+function allowDrop(ev){
+    //Permitir que reciba elementos
+    ev.preventDefault();
+}
+
+function dragStart(ev){
+    //Indicamos qué valor y tipo de información vamos a arrrastrar. En este caso texto e Id del elemento
+    ev.dataTransfer.setData("text",ev.target.id);
+}
+
+function drop(ev){
+
+    console.log(ev);
+    console.log(ev.dataTransfer.getData('text'));
+    console.log(ev.target);
+
+    //Evitamos el comportamiento normal del navegador, que sería abrir el elemento en una nueva pestaña
+    ev.preventDefault();
+
+    //Guardamos el elemento, llamado "text" en una viarable
+    var data = ev.dataTransfer.getData("text");
+
+    //Colgamos el elemento arrastrado y soltado en el nuevo destino.
+    //Si esa caja ya tiene un elemento no se podrá soltar ahí
+    if(!ev.target.draggable){
+        ev.target.appendChild(document.getElementById(data));
+    }
+}
+
+async function init(){
+await recuperarJSON();
+let contenedores = document.querySelectorAll(".contenedorJuego");
+contenedores.forEach(adelante =>{
+    adelante.addEventListener('dragstart',dragStart);
+});
+
+}
+
+window.onload = init;
